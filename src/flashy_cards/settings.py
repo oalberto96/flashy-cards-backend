b45aa084bb1c48b0e8f31758b460f9ca8cdde860
oalberto96 <ochoapublic@gmail.com>
2018-10-25 19:46:38 -0700

    create lessons app

--- src/flashy_cards/settings.py
+++ src/flashy_cards/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication',
     'rest_framework',
     'rest_framework.authtoken',
+    'authentication',
+    'lessons',
 ]
 
 MIDDLEWARE = [
