90d20d15d87c9e4b01d1bedb4768916f61d5a780
oalberto96 <ochoapublic@gmail.com>
2019-01-16 17:21:49 -0800

    add database connection to production settings file

--- src/flashy_cards/settings/production.py
+++ src/flashy_cards/settings/production.py
@@ -90,8 +90,10 @@ WSGI_APPLICATION = 'flashy_cards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "flashycards",
+        "USER": "root",
+        "HOST": "db"
     }
 }
 
